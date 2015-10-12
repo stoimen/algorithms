c07d36bb9b8aeeb2fe4df4624978ae36b4300cde
Stoimen Popov <popov@stoimen.com>
2015-10-12 08:34:00 +0200

    Support only nodes

--- src/js/search/sequential.js
+++ src/js/search/sequential.js
@@ -1,40 +1,11 @@
-(function() {
-
-  var root, sequential;
-
-  if (typeof window == 'object' && this === window) {
-    root = window;
-  }
-  else if (typeof global == 'object' && this === global) {
-    root = global;
-  }
-  else {
-    root = this;
-  }
-
-  sequential = function(list, item) {
-    for (var i = 0, l = list.length; i < l; i++) {
-      if (list[i] == item) {
-        return i;
-      }
+sequential = function(list, item) {
+  for (var i = 0, l = list.length; i < l; i++) {
+    if (list[i] == item) {
+      return i;
     }
-
-    return -1;
-  };
-
-  // Node.js
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = sequential;
-  }
-  // AMD / RequireJS
-  else if (typeof define !== 'undefined' && define.amd) {
-    define([], function() {
-      return sequential;
-    });
-  }
-  // included directly via <script> tag
-  else {
-    root.sequential = sequential;
   }
 
-}());
+  return -1;
+};
+
+module.exports = sequential;
