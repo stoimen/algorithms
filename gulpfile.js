b50bb6273b4c8240cb2fa3aed3a859f6d9ec92b2
Stoimen Popov <popov@stoimen.com>
2015-10-16 10:49:46 +0200

    Drop support of PHP and keep only Javascript

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
 gulp.task('test', function () {
-  return gulp.src('src/js/**/*.test.js')
+  return gulp.src('src/**/*.test.js')
     .pipe(jasmine());
 });
