ac7b1ce230abadb69db0e7ac4447ef0449d99b34
Stoimen Popov <popov@stoimen.com>
2018-02-09 13:16:00 +0100

    moving to es6

--- src/search/sequential.js
+++ src/search/sequential.js
@@ -1,11 +1,1 @@
-sequential = function(list, item) {
-  for (var i = 0, l = list.length; i < l; i++) {
-    if (list[i] == item) {
-      return i;
-    }
-  }
-
-  return -1;
-};
-
-module.exports = sequential;
+module.exports = (list, item) => list.indexOf(item) !== -1
