8fef27656d2ddb911425f1dddbe29523a9bc9d5f
Stoimen Popov <popov@stoimen.com>
2018-02-09 13:18:43 +0100

    move to es6

--- src/minmax/min.js
+++ src/minmax/min.js
@@ -1,13 +1,1 @@
-min = function(list) {
-  var min = list[0];
-
-  for (var i = 1, l = list.length; i < l; i++) {
-    if (min > list[i]) {
-      min = list[i];
-    }
-  }
-
-  return min;
-}
-
-module.exports = min;
+module.exports = (list) => Math.min(...list)
