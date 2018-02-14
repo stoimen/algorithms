5dac670e421bb02ec5a69f9f4e49a15487a2b0c1
Stoimen Popov <stoimen.popov@here.com>
2018-02-14 08:58:54 +0100

    getting rid of gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
-
-gulp.task('test', function () {
-  return gulp.src('test/**/*.spec.js')
-    .pipe(jasmine());
-});
